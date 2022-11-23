72e7df1970241b901a1820e78b66263e01a9c780
woo <woo@DESKTOP-GGF9A2H>
2022-11-23 22:45:58 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include <microsoft.ui.xaml.window.h>
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::App1::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+
+        auto windowNative{ this->try_as<::IWindowNative>() };
+        HWND hWnd{ 0 };
+        windowNative->get_WindowHandle(&hWnd);
+        SetWindowPos(hWnd, NULL, 200, 200, 460, 340, NULL);
+        this->Title(L"MediaPlayerElement 실습");
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+}
